ec1b86c62d161eaefa7d7010ee4029def813aab1
Shanthi Shanmugam <shanthi.shanmugam@gmail.com>
2024-03-06 21:57:40 -0400

    Used fsPromises to read all files in the folder and path to join path and file name. Connected to the database and inserted records to product based on ingredients of each meal making sure only unique values are entered Inserted records to measure table based on the product and meal inserted.

--- importRecipe.ts
+++ importRecipe.ts
@@ -1,9 +1,9 @@
-/** 
+/**
  * import recipe to meal
- * import ingredients 
+ * import ingredients
  * add name into products and include only unique (if not exists)
  * add quantity to measure
- */ 
+ */
 
 // connect to the db
 // load and parse the json file
@@ -13,34 +13,40 @@
 
 import pgp from "pg-promise";
 import fs from "fs";
-import {marked} from "marked";
+import fsPromises from "fs/promises";
+import { marked } from "marked";
+
+// path is required for joining paths across Linux, Windows identifying which / to put
+import path from "path";
 
 type IngredientType = {
-    nameEn: string,
-    quantity: number,
-    unit: string
-}
+  nameEn: string;
+  quantity: number | string | undefined;
+  unit: string | undefined;
+};
 
 type RecipeContentType = {
-    tags: string[], //tags
-  nameEn: string, //name_en
-  method: string, //method
-  portions?: number, //serves
-  prepTime?: number, // not existing
-  cookTime?: number, //cooking_duration
-  tips?: string, //tips
-  ingredients: IngredientType[],
-}
+  tags: string[]; //tags
+  nameEn: string; //name_en
+  method: string; //method
+  portions?: number; //serves
+  prepTime?: number; // not existing
+  cookTime?: number; //cooking_duration
+  tips?: string; //tips
+  ingredients: IngredientType[];
+};
 // code, categories, descriptionEn, nameFr, descriptionFr, photo_url, video_url
 // total_cost, serving_cost, tips, servings_size, servings_size_unit, nutrition_rating
 
 const pgpMain = pgp();
 const db = pgpMain(process.env.DB_URL!);
-const fileName = "recipes" + "/" + "MMS 8 Honey Roasted Root Vegetables.txt.json";
-const data = fs.readFileSync(fileName);
-const recipeContent: RecipeContentType = JSON.parse(data.toString());
 
-const rec = await db.one(`INSERT INTO app.meal(
+const importRecipe = async (fileName) => {
+  console.log("Starting import of ", fileName);
+  const data = fs.readFileSync(fileName);
+  const recipeContent: RecipeContentType = JSON.parse(data.toString());
+  const rec = await db.one(
+    `INSERT INTO app.meal(
     tags,
     name_en,
     method,
@@ -50,22 +56,74 @@ const rec = await db.one(`INSERT INTO app.meal(
     VALUES(
         $1, $2, $3, $4, $5, $6
     ) RETURNING *
-    `, [recipeContent.tags,
-        recipeContent.nameEn,
-        marked.parse(recipeContent.method),
-        recipeContent.portions,
-        recipeContent.cookTime,
-        recipeContent.tips
+    `,
+    [
+      recipeContent.tags,
+      recipeContent.nameEn,
+      marked.parse(recipeContent.method),
+      recipeContent.portions,
+      recipeContent.cookTime,
+      recipeContent.tips,
+    ]
+  );
+
+  for (const ingredient of recipeContent.ingredients) {
+    let prodRec = await db.any("SELECT * FROM app.product where name_en=$1", [
+      ingredient.nameEn,
     ]);
+    let measureRec;
+    let measureProd = prodRec[0];
+    if (prodRec.length === 0) {
+      let prodRecord = await db.one(
+        `INSERT INTO app.product(
+            name_en,
+            code,
+            price,
+            quantity,
+            unit
+        ) VALUES(
+            $1, $2, $3, $4, $5
+        ) RETURNING *`,
+        [ingredient.nameEn, "", 0, 0, ""]
+      );
+      measureProd = prodRecord;
+    }
 
+    // If quantity is not a number (Number.isNaN(NaN) is true while NaN === NaN is false), then set the quantity to 0.
+    const quantity = Number.isNaN(parseInt(ingredient.quantity as string)) ? 0 : parseInt(ingredient.quantity as string);
+    // if ingredient.unit is undefined or set to 0 then unit is assumed as some. This is for salt and pepper.
+    const unit = quantity === 0 ? 'some' : (ingredient.unit || "some");
+    measureRec = await db.one(
+      `INSERT into app.measure(
+            product_id, 
+            meal_id, 
+            quantity,
+            unit
+            )
+            VALUES(
+                $1, $2, $3, $4
+            ) RETURNING *`,
+      [measureProd.id, rec.id, quantity, unit]
+    );
+    console.log("Ingredient:", measureProd.name_en);
+    console.log("quantity: ", measureRec.quantity);
+  }
+};
 
-console.log(rec);
+const recipePath = "recipes-json";
+const importedPath = "recipes-imported";
+let jsonFiles: string[] = [];
+const files = await fsPromises.readdir(recipePath);
+jsonFiles = files.filter(
+  (fName) => path.extname(fName).toLowerCase() === ".json"
+);
+console.log("jsonFiles: ", jsonFiles);
 
-// try {
-//     const meals = await db.any('SELECT * FROM app.meal');
-//     console.log(meals);
-// }
-// catch(e) {
-//     console.error(e);
-// }
-
+for (const jsonFile of jsonFiles) {
+  await importRecipe(path.join(recipePath, jsonFile));
+  fs.renameSync(
+    path.join(recipePath, jsonFile),
+    path.join(importedPath, jsonFile)
+  );
+  console.log("imported: ", jsonFile);
+}
